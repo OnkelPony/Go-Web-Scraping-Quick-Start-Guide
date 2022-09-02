b182e218f93e965eb15cb06a0e0f0ce7df6dce32
Jiří Martínek <zwo@centrum.cz>
2022-09-02 13:23:50 +0200

    Finished Following links - chapter 5.

--- Chapter05/ch5ex1/main.go
+++ Chapter05/ch5ex1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 	"time"
 
@@ -9,27 +10,35 @@ import (
 )
 
 func main() {
-	doc, err := goquery.NewDocument("https://www.packtpub.com/latest-releases")
+	resp, err := http.Get("https://web.archive.org/web/20170223095930/https://www.packtpub.com/latest-releases")
 	if err != nil {
 		panic(err)
 	}
-
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		panic(err)
+	}
 	println("Here are the latest releases!")
 	println("-----------------------------")
 	time.Sleep(1 * time.Second)
-	doc.Find(`div.landing-page-row div[itemtype$="/Product"] a`).
+	doc.Find(`div.book-block-outer div[itemtype$="/Product"] a`).
 		Each(func(i int, e *goquery.Selection) {
 			var title, description, author, price string
 			link, _ := e.Attr("href")
-			link = "https://www.packtpub.com" + link
+			//fmt.Printf("Link: %s", link)
+			link = "https://web.archive.org" + link
 
-			bookPage, err := goquery.NewDocument(link)
+			respPage, err := http.Get(link)
+			if err != nil {
+				panic(err)
+			}
+			bookPage, err := goquery.NewDocumentFromReader(respPage.Body)
 			if err != nil {
 				panic(err)
 			}
 			title = bookPage.Find("div.book-top-block-info h1").Text()
 			description = strings.TrimSpace(bookPage.Find("div.book-top-block-info div.book-top-block-info-one-liner").Text())
-			price = strings.TrimSpace(bookPage.Find("div.book-top-block-info div.onlyDesktop div.book-top-pricing-main-ebook-price").Text())
+			price = strings.TrimSpace(bookPage.Find("div.book-top-pricing-block div.book-top-pricing-price div.book-top-pricing-main-book-price").Text())
 			authorNodes := bookPage.Find("div.book-top-block-info div.book-top-block-info-authors")
 			if len(authorNodes.Nodes) < 1 {
 				return
